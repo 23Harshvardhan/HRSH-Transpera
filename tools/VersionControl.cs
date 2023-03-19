9305c0f503586f2ab08d61e5758e7013d73ff33f
Harshvardhan <23harshvardhan@gmail.com>
2023-03-20 01:16:19 +0530

    Fixed version control.

--- tools/VersionControl.cs
+++ tools/VersionControl.cs
@@ -8,8 +8,8 @@ namespace HRSH_Transpera.tools
 {
     public  class VersionControl
     {
-        public static string clientVersion = "3.0.0";
-        public static string updaterVersion = "1.0.0";
+        public static string clientVersion = "3.0.1";
+        public static string updaterVersion = "2.0.0";
         public static string transperaVersion = "2.0.0";
     }
 }
